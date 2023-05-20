1b8984f268a49d9d827b803f8daa2f16e7000465
cansu kose <cansu.kose@keove.com>
2023-05-20 12:21:28 +0300

    display 2 pages added

--- app/src/main/java/com/example/pdfviewfullscreenproject/MainActivity.kt
+++ app/src/main/java/com/example/pdfviewfullscreenproject/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.pdfviewfullscreenproject
 
+import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.pdf.PdfRenderer
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.example.pdfviewfullscreenproject.databinding.ActivityMainBinding
 import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle
 import com.github.barteksc.pdfviewer.util.FitPolicy
@@ -15,20 +20,47 @@ import java.net.URL
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var previousOrientation = Configuration.ORIENTATION_UNDEFINED
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val pdfUrl = "https://www.africau.edu/images/default/sample.pdf"
-        val tempFile = File.createTempFile("temp", ".pdf", cacheDir)
-        Thread {
-            val pdfByteArray = downloadPDF(pdfUrl)
-            tempFile.writeBytes(pdfByteArray)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            runOnUiThread {
-                displayPDF(tempFile)
-            }
-        }.start()
+        val currentOrientation = resources.configuration.orientation
+        if (previousOrientation != currentOrientation) {
+            previousOrientation = currentOrientation
+
+            val pdfUrl = "https://www.africau.edu/images/default/sample.pdf"
+            val tempFile = File.createTempFile("temp", ".pdf", cacheDir)
+            Thread {
+                val pdfByteArray = downloadPDF(pdfUrl)
+                tempFile.writeBytes(pdfByteArray)
+
+                runOnUiThread {
+                    displayPDF(tempFile)
+                }
+            }.start()
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (newConfig.orientation != previousOrientation) {
+            previousOrientation = newConfig.orientation
+
+            val pdfUrl = "https://www.africau.edu/images/default/sample.pdf"
+            val tempFile = File.createTempFile("temp", ".pdf", cacheDir)
+            Thread {
+                val pdfByteArray = downloadPDF(pdfUrl)
+                tempFile.writeBytes(pdfByteArray)
+
+                runOnUiThread {
+                    displayTwoPages(tempFile)
+                }
+            }.start()
+        }
     }
 
     private fun displayPDF(file: File) {
@@ -73,4 +105,34 @@ class MainActivity : AppCompatActivity() {
 
         return outputStream.toByteArray()
     }
+
+    private fun displayTwoPages(file: File) {
+        val renderer = PdfRenderer(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY))
+        val view1 = createPdfView(renderer, 0)
+        val view2 = createPdfView(renderer, 1)
+
+        view1.layoutParams = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1f)
+        view2.layoutParams = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1f)
+
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.HORIZONTAL
+        layout.addView(view1)
+        layout.addView(view2)
+
+        setContentView(layout)
+    }
+
+    private fun createPdfView(renderer: PdfRenderer, pageNumber: Int): ImageView {
+        val page = renderer.openPage(pageNumber)
+        val bitmap = Bitmap.createBitmap(page.width, page.height, Bitmap.Config.ARGB_8888)
+        page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+
+        val imageView = ImageView(this)
+        imageView.setImageBitmap(bitmap)
+
+        page.close()
+
+        return imageView
+    }
+
 }
